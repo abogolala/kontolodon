bf6652509aae538ceb4088ebb0401793c624aea9
abogolala <119696710+abogolala@users.noreply.github.com>
2022-12-03 06:14:21 +0700

    Update config.js
    1

--- config.js
+++ config.js
@@ -14,31 +14,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285850539404'] //ur owner number
-global.ownername = "Jarot Offc" //ur owner name
-global.developer = "Jarot Offc" //nama lu
-global.ytname = "YT: Jarot offc" //ur yt chanel name
-global.socialm = "https://instagram.com/jarotr_" //ur github or insta name
-global.sgc = "https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL"
-global.location = "Jatim" //ur location
+global.vcardowner = ['6285725753532'] //ur owner number
+global.ownername = "WhyuTzu" //ur owner name
+global.developer = "WhyuTzu" //nama lu
+global.ytname = "Segera" //ur yt chanel name
+global.socialm = "https://instagram.com/WhyuTrue_" //ur github or insta name
+global.sgc = "Segera"
+global.location = "Jateng" //ur location
 
 //bot bomdy 
-global.owner = ['6285850539404'] //ur number
-global.ownernomer = "6285850539404" //ur number
-global.ownertag = '6285850539404' //ur tag number
-global.premium = ['6285850539404'] //ur premium number
-global.botname = '𝘖𝘴𝘤𝘢𝘳-𝘔𝘥' //ur bot name
-global.ownername = "𝘑𝘢𝘳𝘰𝘵 𝘖𝘧𝘧𝘤"
-global.linkz = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //your theme url which will be displayed on whatsapp
-global.dana = "6285850539404"
-global.pulsa = "6285850539404"
-global.websitex = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur website to be displayed
-global.websitex1 = "https://youtube.com/shorts/N6cPd_V-IME?feature=share"
-global.botscript = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ' //script link
+global.owner = ['6285725753532'] //ur number
+global.ownernomer = "6285725753532" //ur number
+global.ownertag = '6285725753532' //ur tag number
+global.premium = ['6285725753532'] //ur premium number
+global.botname = 'Hinakinu' //ur bot name
+global.ownername = "Hinakinu"
+global.linkz = "Segera" //your theme url which will be displayed on whatsapp
+global.dana = "6285725753532"
+global.pulsa = "6285725753532"
+global.websitex = "segera" //ur website to be displayed
+global.websitex1 = "segera"
+global.botscript = 'Rahasia' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Oscar Bot" //ur sticker watermark author
-global.wm = "Jarot ofc" //ur watermark
+global.author = "Hinakinu" //ur sticker watermark author
+global.wm = "Hinakinu" //ur watermark
 
 // Other
 global.sessionName = 'Session'
